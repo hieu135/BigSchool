6998f8b006052fbf0543c8b6132b4384d3af8965
Administrator <Administrator@PC218>
2020-06-19 16:14:08 +0700

    a

--- BigShool/ViewModels/CourseViewModel.cs
+++ BigShool/ViewModels/CourseViewModel.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using BigShool.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace BigShool.ViewModels
 {
     public class CourseViewModel
     {
-        internal List<Category> categories;
+        [Required]
 
         public string Place { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
